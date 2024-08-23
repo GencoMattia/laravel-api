b6f0e455d97a50efbadddb370ba1ec8face398c8
GencoMattia <gencomattia@hotmail.it>
2024-08-23 16:13:08 +0200

    correcter a mispell in api.php

--- routes/api.php
+++ routes/api.php
@@ -21,4 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get("/projects", [ProjectController::class, "index"])->name("api.projects.index");
 
-Route::get("/projects/{project}", [ProjectController::class, "show"])->name("api.projects.show");
+Route::get("/projects/{project}", [ProjectController::class, "show"])->name("api.project.show");
